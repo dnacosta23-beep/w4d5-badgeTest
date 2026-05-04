a0db7a968196198598ad477a7836607d07a3f5b6
Danielle Acosta <dnacosta23@gmail.com>
2026-05-04 11:36:27 -0500

    update files' clear

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Body from './compontents/Body'
+import Footer from './compontents/Footer'
+import Header from './compontents/Header'
+
+function App() {
+
+  return (
+    <>
+    
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+export default App
