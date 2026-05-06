e4de49eee8eb72789896b29b2e55e6ab573ea551
Danielle Acosta <dnacosta23@gmail.com>
2026-05-05 20:45:50 -0500

    graded by Tom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import {useState, useEffect} from 'react'
 import './App.css'
+import { supabase } from './UTILS/supabase.JS'
 
 function App() {
+  const [users, setUsers] = useState ([])
+
+  useEffect (()=> {
+    async function getUsers (){
+      const {data} = await supabase.from ("users").select('*')
+      setUsers(data)
+    }
+    getUsers ()
+  }, [])
+
   return (
     <>
-    
+    {users.map ((user) =>{
+      return <p key= {user.id}>{user.user_name}</p>
+    })}
 
     </>
   )
