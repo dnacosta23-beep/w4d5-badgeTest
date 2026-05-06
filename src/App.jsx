08a7931cf43bc9fa852211efd1e560e01174aece
Danielle Acosta <dnacosta23@gmail.com>
2026-05-05 19:00:34 -0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css'
-import Body from './compontents/Body'
-import Footer from './compontents/Footer'
-import Header from './compontents/Header'
 
 function App() {
-
   return (
     <>
     
-      <Header />
-      <Body />
-      <Footer />
+
     </>
   )
 }
--- src/compontents/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div>
-        <p>copyright 2026</p>
-    </div>
-  )
-}
